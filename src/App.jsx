df1c1b3b34ad63f5d624f8865e3494f7b0284e11
鄭舜仁 <killer80548@gmail.com>
2026-02-13 15:36:06 +0800

    add: post & postId 頁面

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import './App.css';
-import { HashRouter, NavLink, Route, Routes, useNavigate } from 'react-router-dom';
-
+import { HashRouter, NavLink, Route, Routes, useNavigate, Outlet, useParams, Link } from 'react-router-dom';
 
 const Todo = () => {
   return <p>這是 Todo 頁面</p>;
@@ -21,6 +20,23 @@ const Logout = () => {
     </>
   )
 };
+const Post = () => {
+  return (
+    <>
+      <h3>Post</h3>
+      <Link to="5355">PostID:</Link>
+      <Outlet />
+    </>
+  )
+};
+const PostId = () => {
+  const parms = useParams();
+  return (
+    <>
+      {parms.postId}
+    </>
+  )
+}
 
 function App() {
   return (
@@ -42,6 +58,9 @@ function App() {
           <NavLink to="/logout">
             <p>登出 頁面</p>
           </NavLink>
+          <NavLink to="/post">
+            <p>ID 頁面</p>
+          </NavLink>
         </div>
         {/* Routes, Route 練習區 */}
         <Routes>
@@ -49,6 +68,9 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/todo" element={<Todo />} />
           <Route path="/logout" element={<Logout />} />
+          <Route path="/post" element={<Post />}>
+            <Route path=":postId" element={<PostId />} />
+          </Route>
         </Routes>
         {/* 練習區 */}
       </HashRouter>
